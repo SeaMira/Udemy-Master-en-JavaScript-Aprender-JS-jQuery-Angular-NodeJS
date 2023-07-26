664dc507fda38297e739afa1e65c4997b4ef9e5b
Sea <sebastian.mira@gmail.com>
2023-07-26 01:25:03 -0400

    continuacion eventos

--- Javascript/js/30-eventos.js
+++ Javascript/js/30-eventos.js
@@ -8,7 +8,22 @@ function cambiarColor() {
     var bg = boton.style.backgroundColor;
     if (bg === "green") {
         boton.style.backgroundColor = "red";
+        boton.style.padding = "10px 10px 10px 10px";
+        boton.style.border = "1px solid red";
     } else {
         boton.style.backgroundColor = "green";
     }
 }
+
+boton.addEventListener('click', (e) => {
+    cambiarColor();
+    e.preventDefault();
+});
+
+boton.addEventListener('mouseover', (e) => {
+    boton.style.background = "#000000";
+});
+
+boton.addEventListener('mouseout', (e) => {
+    boton.style.background = "#ffffff";
+});
